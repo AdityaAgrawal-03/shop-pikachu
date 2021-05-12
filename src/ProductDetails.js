3d5803721e6009d74ff6bdf9639db5b4d84cf805
Aditya Agrawal <aditya.agrawal0309@gmail.com>
2021-05-12 15:11:41 +0530

    feat: added micro-interaction on buttons

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,16 +1,16 @@
-import { useState } from 'react';
 import { useCart } from './context/cart-context';
 import { useWishlist } from './context/wishlist-context';
-
+ 
 export function ProductDetails({item, setRoute}) {
-  const [cartBtnText, setCartBtnText] = useState("addToCart");
-  const [wishlistBtnText, setWishlistBtnText] = useState("addToWishlist");
 
   const { itemsInCart, setItemsInCart } = useCart();
   const { itemsInWishlist, setItemsInWishlist } = useWishlist();
 
+  const isInCart = itemsInCart.find((cartItem) => cartItem.id === item.id);
+  const isInWishlist = itemsInWishlist.find((wishlistItem) => wishlistItem.id === item.id);
+
   function addToCartHandler(item) {
-    const isInCart = itemsInCart.find((cartItem) => cartItem.id === item.id )
+   
      if (isInCart) {
        return setItemsInCart((previousCart) => previousCart.map((cartItem) => {
          if (cartItem.id === item.id) {
@@ -23,7 +23,7 @@ export function ProductDetails({item, setRoute}) {
   }
 
   function addToWishlistHandler(item) {
-    const isInWishlist = itemsInWishlist.find((wishlistItem) => wishlistItem.id === item.id);
+    
     if (isInWishlist) {
       return setItemsInWishlist((previousWishlist) => previousWishlist.map((wishlistItem) => {
         if (wishlistItem.id === item.id) {
@@ -33,8 +33,9 @@ export function ProductDetails({item, setRoute}) {
     }
     setItemsInWishlist((items) => [...items, {...item, count: 1}])
   }
+  
   return (
-    <div className="card card-shadow card-badge" id={item.id}>
+    <div className="card card-shadow card-badge">
           <span className="badge badge-best-value">Best Value</span>
           <div className="card-header">
             <img src="./assests/bicycle-images/bicycle-1.jpg" alt="bicycle-1" className="card-image" />
@@ -44,40 +45,38 @@ export function ProductDetails({item, setRoute}) {
               <h3>{item.name}</h3></div>
             <div className="card-price">{item.price}</div>
             {
-              wishlistBtnText === 'addToWishlist' ? (
+              isInCart ? (
+                <button className="btn btn-primary" onClick={() => setRoute("cart")}>Go to cart</button>
+                ) : (
+                  <button 
+                    className="btn btn-primary btn-primary-icon-label" 
+                    onClick={() => addToCartHandler(item)}>
+                    <span className="material-icons-outlined md-light">
+                    add_shopping_cart
+                    </span>
+                    Add to Cart
+                 </button>
+                  
+                )
+            }
+            {
+              isInWishlist ? (
+                <button 
+                  className="btn btn-secondary" disabled="true" style={{
+                    cursor: "default"
+                  }}>
+                  Wishlisted
+                </button>
+                
+              ) : (
                 <button 
                   className="btn btn-secondary btn-secondary-icon-label"
-                  onClick={() => {
-                      addToWishlistHandler(item)
-                      setWishlistBtnText("Wishlisted");
-                    }}>
+                  onClick={() => addToWishlistHandler(item)}>
                   <span className="material-icons-outlined md-light">
                     favorite_border
                   </span>
                     Add to Wishlist
                 </button>
-              ) : (
-                <button className="btn btn-secondary" disabled="true" style={{cursor: 'default'}}>Wishlisted</button>
-              )
-            }
-            
-            {
-              cartBtnText === 'addToCart' ? (
-                <button 
-                  className="btn btn-primary btn-primary-icon-label" 
-                  onClick={() => {
-                      addToCartHandler(item)
-                      setCartBtnText("goToCart");
-                    }}>
-                  <span className="material-icons-outlined md-light">
-                    add_shopping_cart
-                  </span>
-                    Add to Cart
-                </button>
-              ) : (
-                <button 
-                  className="btn btn-primary"  
-                  onClick={() => setRoute("cart")}>Go to cart</button>
               )
             }
           </div>
