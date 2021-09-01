542b01a941f89b79e77d73a82ae89c96a8249dab
Aditya Agrawal <aditya.agrawal0309@gmail.com>
2021-09-01 20:45:20 +0530

    style: signup page layout

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -36,47 +36,55 @@ export function Signup() {
         </div>
         <div className="signup-form">
           <form onSubmit={(e) => signupHandler(e)}>
-            <label htmlFor="firstName">First Name</label>
+            <label htmlFor="firstName" className="label">First Name</label>
             <input
               id="firstName"
               name="firstName"
               type="text"
               placeholder="Enter your first name"
               value={firstName}
+              className="input-text input-text-md input-text-form"
               onChange={(e) => setFirstName(() => e.target.value)}
+              required
             />
 
-            <label htmlFor="lastName">Last Name</label>
+            <label htmlFor="lastName" className="label">Last Name</label>
             <input
               id="lastName"
               name="lastName"
               type="text"
               placeholder="Enter your last name"
               value={lastName}
+              className="input-text input-text-md input-text-form"
               onChange={(e) => setLastName(() => e.target.value)}
+              required
             />
 
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email" className="label">Email</label>
             <input
               id="email"
               name="email"
               type="email"
               placeholder="Enter your email address"
               value={email}
+              className="input-text input-text-md input-text-form"
               onChange={(e) => setEmail(() => e.target.value)}
+              required
             />
 
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password" className="label">Password</label>
             <input
               id="password"
               name="password"
               type="password"
               placeholder="Password"
               value={password}
+              className="input-text input-text-md input-text-form"
               onChange={(e) => setPassword(() => e.target.value)}
+              required
             />
 
-            <button type="submit">Signup</button>
+            <button className="btn btn-secondary btn-signup" type="submit">Signup</button>
           </form>
         </div>
       </div>
