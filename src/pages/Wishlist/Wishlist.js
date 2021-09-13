62e3f7a3a9bbbd032a9431b0d300d8d6cac5769c
Aditya Agrawal <aditya.agrawal0309@gmail.com>
2021-09-13 17:20:31 +0530

    added microinteractions

--- src/pages/Wishlist/Wishlist.js
+++ src/pages/Wishlist/Wishlist.js
@@ -1,20 +1,36 @@
 import { useData } from "../../context/DataContext";
 import { WishlistCard } from "../../components/WishlistCard/WishlistCard";
+import { useNavigate } from "react-router-dom";
 import "./Wishlist.css";
 
 export function Wishlist() {
   const {
     state: { wishlist },
   } = useData();
+  const navigate = useNavigate();
 
   return (
     <div className="wishlist-page">
-      <h2 className="wishlist-page-heading">Wishlist</h2>
-      <div className="card-container card-container-wishlist">
-        {wishlist.map((product) => (
-          <WishlistCard product={product} key={product._id} />
-        ))}
-      </div>
+      {wishlist.length ? (
+        <>
+          <h2 className="wishlist-page-heading">Wishlist</h2>
+          <div className="card-container card-container-wishlist">
+            {wishlist.map((product) => (
+              <WishlistCard product={product} key={product._id} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="wishlist-empty">
+          <h2> Wishlist is empty! </h2>
+          <button
+            className="btn btn-primary btn-large"
+            onClick={() => navigate("/")}
+          >
+            Start Shopping!
+          </button>
+        </div>
+      )}
     </div>
   );
 }
